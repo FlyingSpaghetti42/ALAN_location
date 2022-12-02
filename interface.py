314672621620cc422632985e2a89f9184a2b087d
LorenzSchrader <114871756+LorenzSchrader@users.noreply.github.com>
2022-12-02 12:31:59 +0100

    changed input on map

--- interface.py
+++ interface.py
@@ -2,182 +2,203 @@ import streamlit as st
 import folium
 import pandas as pd
 import streamlit_folium as st_folium
-from data.data_engineering import get_location, column_selection, distance_calculation, subcolumn_selection
+from data.data_engineering import get_location, column_selection, distance_calculation
 from data.colors import colors
 from data.distance import manhattan_distance_vectorized
-from routing.dataframe_builder import routing_limitation, df_add_dist_dur, df_transform_dist_dur, df_add_beeline, df_transform_beeline
+from routing.dataframe_builder import routing_limitation, df_add_dist_dur, df_transform_dist_dur
 from routing.geodistances import routing_final
+from routing.utils import speed
 api_key = '5b3ce3597851110001cf62482818c293528942238de6f690d9ec3b11'
 
-try:
-    ##############################################################################
-    ######### Map Layout #########################################################
-    ##############################################################################
+#try:
+##############################################################################
+######### Map Layout #########################################################
+##############################################################################
 
-    #defining the map layout:
-    st.set_page_config(page_title='ALAN',layout="wide", page_icon = ':cry:')
+#defining the map layout:
+st.set_page_config(page_title='ALAN',layout="wide", page_icon = ':cry:')
 
-    ##############################################################################
-    ######### Query Input #########################################################
-    ##############################################################################
-
-
-    # Street Input:
-    address = st.text_input('Adress', 'Please Input the Adress')
-
-    #preferences, the User is able to choose from:
-    preferences = st.selectbox(' Classes', options = ("shop", "office", "highway", "public_transport", "tourism", "amenity", "sport"))
-
-    # Radius Selector:
-    radius = st.slider('Select the Radius', 500, 2000, 2000, 50)
-
-    walker_speed = st.selectbox(' How fast do you walk', options = ("fast", "medium", "slow"))
-
-    checker = st.checkbox('Routing',)
-    def speed():
-        if walker_speed == 'fast':
-            return (7000)
-        elif walker_speed == 'medium':
-            return (4000)
-        elif walker_speed == 'slow':
-            return (3000)
-
-    time_min_km = speed()
-
-    ##############################################################################
-    ######### Data to be used ####################################################
-    ##############################################################################
-
-    #function that gets the location data:
-    location=get_location(address)
-
-    # DataFrame for our class selection:
-
-    if address  != 'Please Input the Adress':
-        @st.cache()
-        def first():
-            return column_selection(location, preferences)
-
-        df_cleaned = first()
-
-    else:
-        df_cleaned = 0
-
-    #st.write(df_cleaned)
-
-    # Subcat Selector:
-    subcolumn = st.multiselect('Preferences', options = (df_cleaned[preferences].unique()))
-
-    #Final DataFrame, that only shows the selected subcolumns:
-    #info_input=subcolumn_selection(df_cleaned, preferences, subcolumn)
-    #st.write(info_input)
-
-    #Renaming the Data to be used in the final frame:
-    #display_data = info_input.rename(columns = {'@lon': 'lon', '@lat':'lat', 'name': 'name'})
-
-    ##############################################################################
-    ### Data Cleaning Step 1 - Classes ###########################################
-    ##############################################################################
-
-    ###### this checks the classes available.
-    #checks_classes = df_cleaned[preferences].unique()
-
-    #defining the data to be displayed in the class data frame. We might be able to use subclass_data.
-    #class_data = info_input[info_input[preferences].isin(checks_classes)].reset_index()
-
-    ##############################################################################
-    ## Data Cleaning Step 2 - Subclasses #########################################
-    ##############################################################################
-
-    subclass_check = list(subcolumn)
-
-    for i in range(len(subcolumn)):
-        subclass_check.append(subcolumn[i])
+##############################################################################
+######### Query Input #########################################################
+##############################################################################
 
 
-    t = df_cleaned[df_cleaned[preferences].isin(subclass_check)]
+# Street Input:
+address = st.text_input('Adress', 'Please Input the Adress')
 
-    display_data = t.rename(columns = {'longitude': 'lon', 'latitude':'lat', 'name': 'name'}).reset_index()
+#preferences, the User is able to choose from:
+preferences = st.selectbox(' Classes', options = ("shop", "office", "highway", "public_transport", "tourism", "amenity", "sport"))
 
-    if checker == False:
-        display_data = distance_calculation(display_data,location,distance=radius)
+# Radius Selector:
+radius = st.slider('Select the Radius', 500, 2000, 2000, 50)
 
-        distance = manhattan_distance_vectorized(location[0],location[1],display_data.lat, display_data.lon)
+walker_speed = st.selectbox(' How fast do you walk', options = ("fast", "medium", "slow"))
 
-        #customize
+checker = st.checkbox('Routing',)
 
-        display_data['walking time'] = display_data.distance.apply(lambda x: (x*60)/time_min_km)
+time_min_km = speed(walker_speed)
 
-        display_data['biking time'] = display_data.distance.apply(lambda x: (x*60)/13000)
+##############################################################################
+######### Data to be used ####################################################
+##############################################################################
 
-        display_data['E-Biking time'] = display_data.distance.apply(lambda x: (x*60)/18000)
+#function that gets the location data:
+location=get_location(address)
 
-        #display_data = subcolumn_selection(df_cleaned,preferences, subclass_check)
+# DataFrame for our class selection:
+if address  != 'Please Input the Adress':
+    @st.cache()
+    def first():
+        return column_selection(location, preferences)
 
-    ##############################################################################
-    ################### Getting Routes  ##########################################
-    ##############################################################################
+    df_cleaned = first()
 
-    else:
-        df = distance_calculation(display_data,location,distance=radius)
-        df = routing_limitation(display_data)
+else:
+    df_cleaned = 0
 
-        dist_walk, dur_walk, dist_cycl_reg, dur_cycl_reg, dist_cycl_e, dur_cycl_e= routing_final(df,location[0],location[1],api_key)
 
-        df = df_add_dist_dur(df,
-                        dist_walk,
-                        dur_walk,
-                        dist_cycl_reg,
-                        dur_cycl_reg,
-                        dist_cycl_e,
-                        dur_cycl_e)
+# Subcat Selector:
+subcolumn = st.multiselect('Preferences', options = (df_cleaned[preferences].unique()))
 
-        display_data = df_transform_dist_dur(df)
+##############################################################################
+### Data Cleaning Step 1 - Classes ###########################################
+##############################################################################
+
+###### this checks the classes available.
+#checks_classes = df_cleaned[preferences].unique()
+
+#defining the data to be displayed in the class data frame. We might be able to use subclass_data.
+#class_data = info_input[info_input[preferences].isin(checks_classes)].reset_index()
+
+##############################################################################
+## Data Cleaning Step 2 - Subclasses #########################################
+##############################################################################
+
+subclass_check = list(subcolumn)
+
+for i in range(len(subcolumn)):
+    subclass_check.append(subcolumn[i])
+
+
+t = df_cleaned[df_cleaned[preferences].isin(subclass_check)]
+
+display_data = t.rename(columns = {'longitude': 'lon', 'latitude':'lat', 'name': 'name'}).reset_index()
+
+if checker == False:
+    display_data = distance_calculation(display_data,location,distance=radius)
+
+    distance = manhattan_distance_vectorized(location[0],location[1],display_data.lat, display_data.lon)
+
+    #customize
+
+    display_data['walking time'] = display_data.distance.apply(lambda x: (x*60)/time_min_km)
+
+    display_data['biking time'] = display_data.distance.apply(lambda x: (x*60)/13000)
+
+    display_data['E-Biking time'] = display_data.distance.apply(lambda x: (x*60)/18000)
+
+##############################################################################
+################### Getting Routes  ##########################################
+##############################################################################
+
+else:
+    df = distance_calculation(display_data,location,distance=radius)
+    df = routing_limitation(display_data)
+
+    dist_walk, dur_walk, dist_cycl_reg, dur_cycl_reg, dist_cycl_e, dur_cycl_e= routing_final(df,location[0],location[1],api_key)
+
+    df = df_add_dist_dur(df,
+                    dist_walk,
+                    dur_walk,
+                    dist_cycl_reg,
+                    dur_cycl_reg,
+                    dist_cycl_e,
+                    dur_cycl_e)
+
+    display_data = df_transform_dist_dur(df)
 
 
 
-    ##############################################################################
-    ################### Displaying Data  #########################################
-    ##############################################################################
+##############################################################################
+################### Displaying Data  #########################################
+##############################################################################
 
 
 
-    #giving each amenity a color:
-    color = colors(subclass_check)
-    #initilaizing our map
-    map = folium.Map(location = location,
-                zoom_start=14,
-                control_scale=True)
+#giving each amenity a color:
+
+color = colors(subclass_check)
+
+#initilaizing our map
+
+map = folium.Map(location = location,
+            zoom_start=14,
+            control_scale=True)
+
+#getting all the selected datapoints into the map (we only display up to 100 datapoints)
 
 
-    #getting all the selected datapoints into the map (we only display up to 100 datapoints)
+for i in range(len(display_data)):
+    html_style = '''
+    <style>
+    table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+    }
+    </style>
+    '''
+    html = f'''
+    {html_style}
+    <p style="font-family: Arial">
+    <table>
+        <tr>
+            <th>Name</th>
+            <td> {display_data['name'][i]} </td>
+        </tr>
+        <tr>
+            <th>Adress</th>
+            <td>{display_data['address'][i]}</td>
+        </tr>
+        <tr>
+            <th>Amenity</th>
+            <td>{display_data['shop'][i]}</td>
+        </tr>
+        <tr>
+            <th> Distance </th>
+            <td>{round(display_data['distance'][i],2)}</td>
+        </tr>
+        <tr>
+            <th> Walking Time </th>
+            <td>{round(display_data['walking time'][i],2)} km</td>
+        </tr>
+    </table>
+    </table>
+    </p>'''
 
+    folium.Marker([display_data.lat[i],
+                display_data.lon[i]],
+                popup = folium.Popup(folium.IFrame(html=html, width=500, height=200), max_width=2000, max_height=500),
+                icon = folium.Icon(color = color[display_data[preferences][i]],
+                                    icon = 'info-sign')).add_to(map)
 
-    for i in range(len(display_data)):
-        folium.Marker([display_data.lat[i],
-                    display_data.lon[i]],
-                    popup=display_data.name[i],
-                    icon = folium.Icon(color = color[display_data[preferences][i]],
-                                        icon = 'info-sign')).add_to(map)
+    folium.Marker([location[0],location[1]],
+                popup = '<b>You</b>',
+                icon = folium.Icon(color='blue', icon='alien', prefix = 'fa')).add_to(map)
 
-        folium.Marker([location[0],location[1]],
-                    popup = '<b>You</b>',
-                    icon = folium.Icon(color='blue', icon='alien', prefix = 'fa')).add_to(map)
-
-    #creating a radius:
-    folium.Circle(
-    location=location,
-    radius=radius, #hardcoded for now
-    popup=f"{radius}m Radius", #hardcoded for now
-    color="#3186cc",
-    fill=True,
-    fill_color="#3186cc",
+#creating a radius:
+folium.Circle(
+location=location,
+radius=radius, #hardcoded for now
+popup=f"{radius}m Radius", #hardcoded for now
+color="#3186cc",
+fill=True,
+fill_color="#3186cc",
 ).add_to(map)
 
 
-    #displaying the map:
-    st_folium.folium_static(map, width = 1600, height = 1000)
-    st.dataframe(display_data, width = 1600)
+#displaying the map:
+st_folium.folium_static(map, width = 1600, height = 1000)
+st.dataframe(display_data, width = 1600)
 
-except:
-    pass
+#except:
+#pass
--- routing/utils.py
+++ routing/utils.py
@@ -8,3 +8,11 @@ def transform_min(float_min):
     float_min_min = str(int(str(float_min).split('.')[0]))
     str_min = f'{float_min_min} min {float_min_s} s'
     return str_min
+
+def speed(walker_speed):
+    if walker_speed == 'fast':
+        return (7000)
+    elif walker_speed == 'medium':
+        return (4000)
+    elif walker_speed == 'slow':
+        return (3000)
