e3c221d79448c462da3a5385e4b0ef96156e31af
LorenzSchrader <114871756+LorenzSchrader@users.noreply.github.com>
2022-12-02 15:38:39 +0100

    before merging

--- interface.py
+++ interface.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import folium
-import pandas as pd
+from folium import plugins
 import streamlit_folium as st_folium
 from data.data_engineering import get_location, column_selection, distance_calculation
 from data.colors import colors
@@ -8,6 +8,7 @@ from data.distance import manhattan_distance_vectorized
 from routing.dataframe_builder import routing_limitation, df_add_dist_dur, df_transform_dist_dur
 from routing.geodistances import routing_final
 from routing.utils import speed
+
 api_key = '5b3ce3597851110001cf62482818c293528942238de6f690d9ec3b11'
 
 #try:
@@ -95,7 +96,7 @@ if checker == False:
 
     display_data['biking time'] = display_data.distance.apply(lambda x: (x*60)/13000)
 
-    display_data['E-Biking time'] = display_data.distance.apply(lambda x: (x*60)/18000)
+    display_data['Car Travel time'] = display_data.distance.apply(lambda x: (x*60)/18000)
 
 ##############################################################################
 ################### Getting Routes  ##########################################
@@ -136,17 +137,18 @@ map = folium.Map(location = location,
             control_scale=True)
 
 #getting all the selected datapoints into the map (we only display up to 100 datapoints)
-
+#defining the html style of the boxes on the map
+html_style = '''
+<style>
+table, th, td {
+border: 1px solid black;
+border-collapse: collapse;
+}
+</style>
+'''
 
 for i in range(len(display_data)):
-    html_style = '''
-    <style>
-    table, th, td {
-    border: 1px solid black;
-    border-collapse: collapse;
-    }
-    </style>
-    '''
+
     html = f'''
     {html_style}
     <p style="font-family: Arial">
@@ -165,16 +167,15 @@ for i in range(len(display_data)):
         </tr>
         <tr>
             <th> Distance </th>
-            <td>{round(display_data['distance'][i],2)}</td>
+            <td>{round(display_data['distance'][i],2)} metres</td>
         </tr>
         <tr>
             <th> Walking Time </th>
-            <td>{round(display_data['walking time'][i],2)} km</td>
+            <td>{round(display_data['walking time'][i],2)} minutes </td>
         </tr>
     </table>
     </table>
     </p>'''
-
     folium.Marker([display_data.lat[i],
                 display_data.lon[i]],
                 popup = folium.Popup(folium.IFrame(html=html, width=500, height=200), max_width=2000, max_height=500),
@@ -182,8 +183,22 @@ for i in range(len(display_data)):
                                     icon = 'info-sign')).add_to(map)
 
     folium.Marker([location[0],location[1]],
-                popup = '<b>You</b>',
+                popup = folium.Popup(html=f'''{html_style}
+                <p style="font-family: Arial">
+                <table>
+                    <tr>
+                        <th>You</th>
+                    </tr>
+                    <tr>
+                        <th>Current Location:</th>
+                        <td>{address}</td>
+                    </tr>
+                </table>
+                </p>
+                ''',width=200, height=100),
                 icon = folium.Icon(color='blue', icon='alien', prefix = 'fa')).add_to(map)
+
+
 
 #creating a radius:
 folium.Circle(
@@ -195,10 +210,47 @@ fill=True,
 fill_color="#3186cc",
 ).add_to(map)
 
-
 #displaying the map:
+fmtr = "function(num) {return L.Util.formatNum(num, 3);};"
+
+#map pointer, checks where the position is on map:
+
+
+#loc = plugins.MousePosition(position='topright', separator=' , ',numDigits = 6,
+#              lat_formatter=fmtr, lng_formatter=fmtr).add_to(map)
+
+
 st_folium.folium_static(map, width = 1600, height = 1000)
+
+#def color_coding(df):
+#    return ['background-color:red'] * len(
+#        df) if df.lon == loc
+#
+#    if loc == (display_data.lon , display_data.lat):
+#        return ['background-color:green']
+#    else: pass
+
+
+
 st.dataframe(display_data, width = 1600)
+
+@st.experimental_memo
+def convert_df(df):
+   return df.to_csv(index=False).encode('utf-8')
+
+
+csv = convert_df(display_data)
+
+st.download_button(
+   "Press to Download",
+   csv,
+   "file.csv",
+   "text/csv",
+   key='download-csv'
+)
+
+
+
 
 #except:
 #pass
