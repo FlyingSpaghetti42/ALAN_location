1de402b1d9d975cf3c3fbed8f5e74e9f209414b4
Niklas Peter <niklasapeter@gmail.com>
2022-12-05 10:06:15 +0100

    more advanced data cleaning

--- data/data_engineering.py
+++ data/data_engineering.py
@@ -7,6 +7,8 @@ from geopy.distance import geodesic
 def get_location(address):
     '''
     Function takes address as input and returns latitude and longitude
+
+    address: string in the form: 'street, house number, city, (country)'
     '''
     from geopy.geocoders import Nominatim
     geolocator = Nominatim(user_agent='default')
@@ -40,33 +42,146 @@ def get_complete_data(location, radius=2000):
     rawData = pd.read_csv(io.StringIO(r.decode('utf-8')), delimiter='\t')
     return rawData
 
+def raw_data(address:str, radius=2000):
+    location_lat,location_lon = get_location(address)
+    return get_complete_data((location_lat,location_lon),radius)
 
-def column_selection(location,column_name):
+def data_cleaning(address:str, radius=2000):
     '''
-    Function cleans the data based on column selection
+    This function uses the above raw_data() function to make the
+    overpass api call to get the data for all nodes within the
+    specified radius.
+
+    It then proceeds to apply various data cleaning operations to get
+    a nice looking dataframe, which can be used for further analyses.
+
+    Parameters:
+    address: string of the address of concern for optimale results best
+            passed in the form: 'street,house no.,city, country'
+    radius: integer specifying in metres the radius around the location
+            that is to be searched
     '''
-    df=get_complete_data(location)
-    list_columns=[ "shop", "office", "highway", "public_transport", "tourism", "amenity", "sport"]
-    list_address=['name','addr:street','addr:housenumber','addr:suburb','addr:city','addr:postcode',
-                  'addr:country','contact:phone','contact:website']
-    df_amenity= df.dropna(subset=[column_name, 'name'])
-    df_amenity.rename(columns={"@lon":"longitude", "@lat":"latitude"}, inplace=True)
-    list_columns.remove(column_name)
-    df_amenity.drop(columns=list_columns, inplace=True)
-    df_amenity.fillna(" ",inplace=True)
-    df_amenity["address"]=df_amenity[list_address].astype(str).apply(",".join, axis=1)
-    df_amenity["address"]=df_amenity["address"].apply(lambda x: x.strip(', '))
+    # get the data:
+    df = raw_data(address,radius=2000)
+
+    # Remove rows with no entries for none of categories:
+    df.dropna(subset=["shop", "office", "highway",
+                         "public_transport", "tourism",
+                         "amenity", "sport"], how='all', inplace=True)
+    df.dropna(subset=["name"], how='any', inplace=True)
+
+    # Operations on the addresses to get one column containing the address:
+    ## list of address related columns
+    list_address = ['name','addr:street',
+                    'addr:housenumber','addr:suburb',
+                    'addr:city','addr:postcode',
+                    'addr:country']
+    ## Merge the address columns
+    df.fillna(" ",inplace=True)
+    df['addr:postcode'] = df['addr:postcode'].astype(str).apply(lambda x: x.replace('.0',''))
+    df["Address"] = df[list_address].astype(str).apply(",".join, axis=1)
+    df["Address"] = df["Address"].apply(lambda x: x.strip(', '))
     list_address.remove('name')
-    df_amenity.drop(columns=list_address, inplace=True)
-    df_amenity.reset_index(inplace=True, drop=True)
-    return df_amenity
+    df.drop(columns = list_address, inplace = True)
+
+    # Create new column merging highway and traffic category
+    df['Traffic'] = df[['highway',
+                            'public_transport']].astype(str).apply(",".join, axis=1)
+    ## remove redundant columns
+    df.drop(columns = ['highway', 'public_transport', '@id'], inplace = True)
+
+    # Rename columns:
+    ## dict of columns to be renamed
+    column_rename = {
+        "shop": 'Shopping',
+        "office": 'Office',
+        "tourism": 'Tourism',
+        "amenity": 'Amenity',
+        "sport": 'Sports',
+        "@lat": 'Latitude',
+        "@lon": 'Longitude',
+        "name": 'Location Name',
+        "contact:website": 'Website',
+        "contact:phone": 'Phone Number'
+    }
+    ## rename the columns using the above dict
+    df.rename(columns = column_rename, inplace = True)
+
+    # Data cleaning subclasses:
+
+    ## Get rid of Location Name in Address line and format Address
+    df['Address'] = df.apply(lambda df: df['Address'].replace(df['Location Name'],''),axis=1)
+    ### Remove leading or trailing commas
+    df['Address'] = df['Address'].apply(lambda x: x.strip(','))
+    ### Add whitespace after comma
+    df['Address'] = df['Address'].apply(lambda x: x.replace(',',' '))
+
+    ## String formatting of Subclass Names:
+    cols = ['Amenity', 'Traffic', 'Shopping',
+            'Office', 'Tourism', 'Sports']
+    ### clear commas
+    df[cols] = df[cols].apply(lambda x: x.str.replace(',',' '))
+    ### clear leading and trailing whitespace
+    df[cols] = df[cols].apply(lambda x: x.str.strip())
+    ### replace snake format with whitespace sep
+    df[cols] = df[cols].apply(lambda x: x.str.replace('_',' '))
+    ### capitalize
+    df[cols] = df[cols].apply(lambda x: x.str.capitalize())
+
+    # rearrange columns
+    df = df.reindex(columns=['Location Name','Address','Website','Phone Number',
+                             'Latitude', 'Longitude',
+                             'Amenity', 'Traffic',
+                             'Shopping', 'Office',
+                             'Tourism', 'Sports'])
+
+    # reset index
+    df.reset_index(inplace=True, drop = True)
+    return df
+
+def filter_columns(df, column_name):
+    '''
+    Replaces the column_selection function
+
+    Filters the preprocessed DataFrame to only include the category chosen
+    (Shopping Facility, Office, Traffic, Public Transport, Tourism, Amenity,
+    Sport Facility)
+    '''
+    list_columns = ['Shopping',
+                    'Office', 'Traffic','Tourism',
+                    'Amenity', 'Sports']
+    list_columns.remove(column_name)
+    df.drop(columns = list_columns, inplace = True)
+    df = df[df[column_selection] != '']
+    df.reset_index(inplace=True, drop = True)
+    return df
+
+#def column_selection(location,column_name):
+#    '''
+#    Function cleans the data based on column selection
+#    '''
+#    df=get_complete_data(location)
+#    list_columns=[ "shop", "office", "highway", "public_transport", "tourism", "amenity", "sport"]
+#    list_address=['name','addr:street','addr:housenumber','addr:suburb','addr:city','addr:postcode',
+#                  'addr:country','contact:phone','contact:website']
+#    df_amenity= df.dropna(subset=[column_name, 'name'])
+#    df_amenity.rename(columns={"@lon":"longitude", "@lat":"latitude"}, inplace=True)
+#    list_columns.remove(column_name)
+#    df_amenity.drop(columns=list_columns, inplace=True)
+#    df_amenity.fillna(" ",inplace=True)
+#    df_amenity["address"]=df_amenity[list_address].astype(str).apply(",".join, axis=1)
+#    df_amenity["address"]=df_amenity["address"].apply(lambda x: x.strip(', '))
+#    list_address.remove('name')
+#    df_amenity.drop(columns=list_address, inplace=True)
+#    df_amenity.reset_index(inplace=True, drop=True)
+#    return df_amenity
 
 
 def subcolumn_selection(df, column_name,subcolumn_name):
     '''
     Function to update the dateframe with sub column selection
     '''
-    return df[df[column_name]in subcolumn_name]
+    return df[df[column_name] in subcolumn_name]
 
 def distance_calculation(df,location,distance=2000):
     '''
